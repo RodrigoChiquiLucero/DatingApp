2dfd2a57e1acaaa46331caa92b7da024fcfa6fdd
luceror <luceror@avaya.com>
2020-10-14 18:14:09 -0300

    End of section 9

--- client/src/app/members/member-list/member-list.component.ts
+++ client/src/app/members/member-list/member-list.component.ts
@@ -1,3 +1,5 @@
+import { MembersService } from './../../_services/members.service';
+import { Member } from './../../_models/member';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,10 +9,18 @@ import { Component, OnInit } from '@angular/core';
   ]
 })
 export class MemberListComponent implements OnInit {
+  members: Member[];
 
-  constructor() { }
+  constructor(private memberService: MembersService) { }
 
   ngOnInit(): void {
+    this.loadMembers();
+  }
+
+  loadMembers() {
+    this.memberService.getMembers().subscribe(members => {
+      this.members = members;
+    });
   }
 
 }
